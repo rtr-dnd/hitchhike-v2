20e6acd5e1fc5396fa25e1b69cd5a04352c2937c
rtr-dnd <skysoyn@gmail.com>
2023-11-13 13:00:53 +0900

    temp commit

--- Assets/NetcodeHitchhike/Scripts/PlayerMovementPool.cs
+++ Assets/NetcodeHitchhike/Scripts/PlayerMovementPool.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Oculus.Interaction;
 
 public class PlayerMovementPool : NetworkBehaviour
 {
+    public GameObject hmdPrefab;
+    GameObject headVisual; // todo: divide into dedicated script
     public NetworkVariable<NetworkHandJointPoses> leftJointsPool = new NetworkVariable<NetworkHandJointPoses>(
         default,
         NetworkVariableReadPermission.Everyone,
@@ -15,4 +18,20 @@ public class PlayerMovementPool : NetworkBehaviour
         NetworkVariableReadPermission.Everyone,
         NetworkVariableWritePermission.Owner
     );
+    public NetworkVariable<NetworkPose> hmdPosePool = new NetworkVariable<NetworkPose>(
+        default,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner
+    );
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        headVisual = Instantiate(hmdPrefab);
+    }
+
+    private void Update()
+    {
+        headVisual.transform.SetPose(hmdPosePool.Value);
+    }
 }
