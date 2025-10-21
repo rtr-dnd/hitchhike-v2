f8f3760ada1344cc95d62563c9a050e26bdbc72f
Naoki Fujimoto <nao-nao-7070@g.ecc.u-tokyo.ac.jp>
2025-10-21 13:58:47 +0900

    debug7

--- Assets/NetcodeHitchhike/BlockTask/Scripts/TaskImageManager.cs
+++ Assets/NetcodeHitchhike/BlockTask/Scripts/TaskImageManager.cs
@@ -1,80 +1,83 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Unity.Netcode;
+
 public class TaskImageManager : NetworkBehaviour
 {
-
     [SerializeField] private SpriteRenderer imageForClient0;
     [SerializeField] private SpriteRenderer imageForClient1;
     [SerializeField] private List<Sprite> taskImages;
-    //[SerializeField] private int imageIndex = 0;
-    [SerializeField] NetworkVariable<int> imageIndex = new NetworkVariable<int>(
+
+    // 書き込み権限をOwnerからServerに変更
+    private NetworkVariable<int> imageIndex = new NetworkVariable<int>(
         0,
         NetworkVariableReadPermission.Everyone,
-        NetworkVariableWritePermission.Server
+        NetworkVariableWritePermission.Server // サーバーのみが書き込めるように変更
     );
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public override void OnNetworkSpawn()
     {
+        // OnValueChangedイベントにコールバックを登録
         imageIndex.OnValueChanged += OnImageIndexChanged;
+
+        // サーバーのみが初期値を設定する
         if (IsServer)
         {
             imageIndex.Value = 0;
         }
 
+        // 登録後、現在の値でUIを一度更新する（全クライアントで実行）
+        // これにより、途中参加したクライアントも正しいスプライトが表示される
         OnImageIndexChanged(0, imageIndex.Value);
     }
     
-    private void OnImageIndexChanged(int previousValue, int newValue)
+    public override void OnNetworkDespawn()
     {
-        if (newValue < 0 || newValue >= taskImages.Count * 2 + 1)
-        {
-            Debug.LogError("Invalid image index: " + newValue);
-            return;
-        }
-        imageForClient0.sprite = taskImages[newValue * 2];
-        imageForClient1.sprite = taskImages[newValue * 2 + 1];
+        // オブジェクトが破棄される際にイベントの購読を解除
+        imageIndex.OnValueChanged -= OnImageIndexChanged;
     }
 
-    // Update is called once per frame
+    // OnValueChangedで呼び出されるメソッド
+    private void OnImageIndexChanged(int previousValue, int newValue)
+    {
+        // インデックスがリストの範囲外にならないようにチェック
+        if (taskImages.Count > newValue * 2 + 1)
+        {
+            imageForClient0.sprite = taskImages[newValue * 2];
+            imageForClient1.sprite = taskImages[newValue * 2 + 1];
+            Debug.Log($"Task image changed. New index: {newValue}");
+        }
+        else
+        {
+            Debug.LogWarning("Task image index is out of range!");
+        }
+    }
+
     void Update()
     {
-        if (!IsServer) return;
-        if (Input.GetKeyDown(KeyCode.Space))
+        // サーバー（ホスト）でなければ入力処理を受け付けない
+        if (!IsServer)
         {
-            if (imageIndex.Value == 5)
-            {
-                imageIndex.Value = 0;
-                return;
-            }
-            imageIndex.Value++;
+            return;
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            imageIndex.Value = 0;
+            int nextIndex = imageIndex.Value + 1;
+            // 6になる場合は0に戻す (仮にタスクが6ペアある場合)
+            if (nextIndex >= 6) 
+            {
+                nextIndex = 0;
+            }
+            imageIndex.Value = nextIndex;
         }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            imageIndex.Value = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            imageIndex.Value = 2;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            imageIndex.Value = 3;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            imageIndex.Value = 4;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            imageIndex.Value = 5;
-        }
+
+        // 数字キーでの直接指定 (こちらは簡略化のためそのまま)
+        if (Input.GetKeyDown(KeyCode.Alpha0)) imageIndex.Value = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) imageIndex.Value = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) imageIndex.Value = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) imageIndex.Value = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha4)) imageIndex.Value = 4;
+        if (Input.GetKeyDown(KeyCode.Alpha5)) imageIndex.Value = 5;
     }
 }
