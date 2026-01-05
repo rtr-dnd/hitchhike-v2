a84f9eb12777d602afce4f2c0910973e082dca8e
Naoki Fujimoto <nao-nao-7070@g.ecc.u-tokyo.ac.jp>
2026-01-05 16:47:03 +0900

    itityousei

--- Assets/NetcodeHitchhike/Demo/Scripts/DemoTitle.cs
+++ Assets/NetcodeHitchhike/Demo/Scripts/DemoTitle.cs
@@ -36,7 +36,7 @@ public class DemoTitle : MonoBehaviour
         
         var position = new Vector3(0, 0, -20f)
         {
-            x =  (NetworkManager.Singleton.ConnectedClients.Count % 3) * 2f
+            x = -1 + (NetworkManager.Singleton.ConnectedClients.Count % 3) * 2f
         };
         response.Position = position;
         response.Rotation = Quaternion.identity;
